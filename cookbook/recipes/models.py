6001257b790c088d3df35017d106bdd0d2691c0d
Eugen Lindorfer <eugen.lindorfer@lindy.net>
2022-10-15 16:49:42 +0200

    Add: working data model

--- cookbook/recipes/models.py
+++ cookbook/recipes/models.py
@@ -1,9 +1,66 @@
 from django.db import models
 
+from wagtail.core import blocks
 from wagtail.models import Page
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, StreamFieldPanel
 from wagtail.search import index
+from wagtail.core.fields import StreamField
+from wagtail.core.blocks import StreamBlock
+
+
+class SectionBlock(blocks.StreamBlock):
+    ingredients = StreamBlock([
+        ('name', blocks.TextBlock(
+            help_text="Zutat",
+            required=True,
+        )),
+        ('amount', blocks.FloatBlock(
+            label="Menge",
+            help_text="Menge",
+            required=False,
+            min_value=0.0,
+            max_value=999999.0,
+        )),
+        ('unit', blocks.ChoiceBlock(
+            label="Einheit",
+            required=False,
+            choices=[
+                ('el', 'Esslöffel'),
+                ('tl', 'Teelöffel'),
+            ]
+        )),
+    ],
+        use_json_field=False,
+        blank=True,
+    )
+
+    instructions = RichTextField(blank=True)
+
+    class Meta:
+        icon = "form"
+        label = "Rezeptabschnitt hinzufügen"
+
+
+class RecipePage(Page):
+    sections = StreamField([
+        ('sections', SectionBlock()),
+    ],
+        use_json_field=False,
+        blank=False,
+    )
+
+    comments = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('sections'),
+        index.SearchField('comments'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('sections'),
+        FieldPanel('comments'),
+    ]
 
 
 class RecipeIndexPage(Page):
@@ -11,19 +68,4 @@ class RecipeIndexPage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('intro')
-    ]
-
-
-class RecipePage(Page):
-    instructions = RichTextField(blank=True)
-    comments = RichTextField(blank=True)
-
-    search_fields = Page.search_fields + [
-        index.SearchField('instructions'),
-        index.SearchField('comments'),
-    ]
-
-    content_panels = Page.content_panels + [
-        FieldPanel('instructions'),
-        FieldPanel('comments'),
     ]
