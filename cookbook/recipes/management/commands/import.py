a7001a8c624d9935625175ab00343e4020d9568a
Eugen Lindorfer <eugen.lindorfer@lindy.net>
2022-10-25 10:22:28 +0200

    Fix: Importer - still need debugging

--- cookbook/recipes/management/commands/import.py
+++ cookbook/recipes/management/commands/import.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from django.core.management.base import BaseCommand
 from wagtail.core.models import Page
 from recipes.models import RecipeIndexPage, RecipePage
@@ -10,17 +11,42 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         home = Page.objects.get(id=10)
 
-
+        ingredients = []
+        oldtitle = ""
 
         reader = csv.DictReader(open("../data/gourmet_export.csv"), delimiter='|', quotechar='"')
         for row in reader:
-            recipe_page = RecipePage(
-                
-            )
+            if row["title"] == oldtitle:
+                ingredients.append(
+                    {
+                        'name': row["item"],
+                        'amount': row["amount"],
+                        'unit': row["unit"],
+                    }
+                )
 
+            else:
+                oldtitle = row["title"]
 
-            home.add_child(instance=RecipePage)
+                section1 = {
+                    'heading': "",
+                    'ingredients': ingredients,
+                    'instructions': "mytext",
+                }
 
-            title = row["title"]
-            print(title)
+                recipe_page = RecipePage(
+                    title=row["title"],
+                    portions=4,  # TODO change this
+                    sections=json.dumps([
+                        {
+                            'type': "sections",
+                            'value': section1
+                        },
+                    ]),
+                    comments=row["modifications"]
+                )
 
+                print(ingredients)
+
+                #home.add_child(instance=recipe_page)
+                #recipe_page.save_revision().publish()
