fdd6856fbdd14d2e2237df688752d7ecc4efaeb1
Eugen Lindorfer <eugen.lindorfer@lindy.net>
2022-10-28 18:10:36 +0200

    Add: Settings

---
+++ cookbook/cookbook/settings/dev.py
@@ -0,0 +1,13 @@
+from decouple import config
+
+from .base import *
+
+DEBUG = True
+SECRET_KEY = config("SECRET_KEY")
+ALLOWED_HOSTS = ["*"]
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+try:
+    from .local import *
+except ImportError:
+    pass
