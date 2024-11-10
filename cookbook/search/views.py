9be819c22658fcafc485e4a2d983422db302afc0
Eugen Lindorfer <eugen.lindorfer@lindy.net>
2024-11-10 12:53:45 +0100

    Add: Order search results by title.

--- cookbook/search/views.py
+++ cookbook/search/views.py
@@ -7,7 +7,8 @@ def search(request):
     search_query = request.GET.get('search', None)
 
     if search_query:
-        search_results = RecipePage.objects.live().search(search_query)
+        search_results = RecipePage.objects.live().order_by(
+            'title').search(search_query, order_by_relevance=False)
         print(search_results)
         query = Query.get(search_query)
         query.add_hit()
